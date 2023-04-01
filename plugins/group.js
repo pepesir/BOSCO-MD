cce53aefbc8e2a8a1658b8dc146b1423401ba0df
Roshan <90826638+pepesir@users.noreply.github.com>
2023-04-01 18:01:54 +0530

    Create group.js

---
+++ plugins/group.js
@@ -0,0 +1,44 @@
+const Bot = require('../lib/events');
+Bot.addCMD(
+	{
+		pattern: 'jid',
+		desc: 'get user/group jid',
+		isOwner: false,
+		type: 'misc'
+},
+async (conn, match) => {
+	await conn.reply(conn.chatId)		
+	});
+
+Bot.addCMD(
+	{
+		pattern: 'gjid',
+		desc: 'gets jid of all group members',
+		isOwner: false,
+		type: 'group'
+},
+async (conn, match) => {
+	if (!conn.chatId.includes("@g.us"))
+        return await conn.reply("Only works in groups.")
+	let { participants } = await conn.groupMetadata(conn.chatId);
+	let participant = participants.map((u) => u.id);
+	let str = "┌─────〔 𝖌𝖗𝖔𝖚𝖕 𝖏𝖎𝖉𝖘 〕\n";
+	participant.forEach((result) => {
+		str += `┊ ${result}\n`;
+		});
+		str += `└────────────────────𔒝`;
+		conn.reply(str);	
+	});
+
+Bot.addCMD(
+	{
+		pattern: 'leave',
+		desc: 'leave a group',
+		isOwner: true,
+		type: 'group'
+},
+async (conn, match) => {
+	if (!conn.chatId.includes("@g.us")) 
+	return await conn.reply("Only works in groups.")
+	await conn.groupLeave(conn.chatId)
+	});
